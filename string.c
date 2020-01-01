c380590e81de61476112666c00e6e15fd8efd44e
Izzy <yzziizzy@gmail.com>
2020-01-01 15:11:26 -0700

    in-place string split

--- string.c
+++ string.c
@@ -2,6 +2,7 @@
 
 
 #include <string.h>
+
 #include "string.h"
 
 
@@ -52,4 +53,103 @@ char* strappend(const char* a, const char* const b) {
 	o[la + lb] = '\0';
 	return o;
 }
+
+
+
+// returns a null-terminated list of pointers to each line.
+// mutates the source (replaces newlines with nulls)
+char** strsplit_inplace(char* src, char delim, size_t* outLen) {
+	size_t alloc = 8;
+	size_t len = 0;
+	char** out = malloc(alloc * sizeof(*out));
+	
+	char* start = src;
+	
+	
+	for(size_t i = 0; src[i] != 0; i++) {
+		if(src[i] == delim) {
+			src[i] = 0; // put in a null terminator
+			
+			
+			out[len++] = start;
+			start = src + i + 1;
+			
+			// always have an extra 
+			if(len >= alloc){
+				alloc *= 2;
+				out = realloc(out, alloc * sizeof(*out));
+			}
+			
+		}
+	}
+	
+	out[len++] = start;
+	
+	if(outLen) *outLen = len;
+	
+	return out;
+}
+
+
+
+
+
+int decodeHexDigit(char c) {
+	if(c >= '0' && c <= '9') {
+		return c - '0';
+	}
+	else if(c >= 'a' && c <= 'f') {
+		return 10 + (c - 'a');
+	}
+	else if(c >= 'A' && c <= 'F') {
+		return 10 + (c - 'A');
+	}
+	return 0;
+}
+/*
+static double nibbleHexNorm(char* s) {
+	if(s[0] == '\0' || s[1] == '\0') return 0.0;
+	double d = (decodeHexDigit(s[0]) * 16.0) + decodeHexDigit(s[1]);
+	return d / 256.0;
+}
+
+Vector4 sexp_argAsColor(sexp* x, int argn) {
+	int i;
+	union {
+		Vector4 c;
+		float f[4];
+	} u;
+	
+	u.c.x = 0.0;
+	u.c.y = 0.0;
+	u.c.z = 0.0;
+	u.c.w = 1.0; // default alpha is 1.0 
+
+	if(VEC_LEN(&x->args) < argn) return u.c;
+	sexp* arg = VEC_ITEM(&x->args, argn);
+	
+	if(arg->type == 0) { // it's an s-expression
+		for(i = 0; i < VEC_LEN(&arg->args); i++) { 
+			u.f[i] = sexp_argAsDouble(arg, i);
+		}
+	}
+	else { // it's a literal
+		// throw away any leading BS
+		char* s = arg->str;
+		char* e = arg->str + strlen(arg->str);
+		if(s[0] == '#') s++;
+		if(s[0] == '0' && (s[1] == 'x' || s[1] == 'X')) s += 2;
+		
+		for(i = 0; i < 4 && s < e; i++) {
+			u.f[i] = nibbleHexNorm(s);
+			s += 2;
+		}
+	}
+	
+	printf("color: %f,%f,%f,%f\n", u.c.x, u.c.y, u.c.z, u.c.w);
+	
+	return u.c;
+}
+*/
+
 
--- string.h
+++ string.h
@@ -13,10 +13,20 @@ char* strlndup(const char* s);
 // line count;
 size_t strlinecnt(const char* s);
 
-
 // append b to a in a new buffer
 char* strappend(const char* a, const char* const b);
 
+// returns a null-terminated list of pointers to each line.
+// mutates the source (replaces newlines with nulls)
+char** strsplit_inplace(char* src, char delim, size_t* outLen);
+
+
+// TODO:
+// trim whitespace
+// collapse whitespace
+// collapse ws to a single ' '
+// VA fn to join arbitrary number of strings
+// compare functions for natural interpretations of strings (version sort)
 
 
 #endif // __sti__string_h__
