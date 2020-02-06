3266bd71a107f97145bdc95b5547b7d229b0c830
Izzy <yzziizzy@gmail.com>
2020-02-06 13:50:20 -0700

    start of iprintf()

--- string.c
+++ string.c
@@ -1,6 +1,6 @@
 // Public Domain.
 
-
+#include <stdarg.h>
 #include <string.h>
 
 #include "string.h"
@@ -164,3 +164,292 @@ void decodeHexColorNorm(char* s, float* out) {
 	}
 }
 
+
+
+
+/*
+Garbage in, garbage out.
+Unsigned ints only.
+Dont't give a base of 1.
+Dont't give a base greater than 36.
+Give a sufficiently long buffer. You figure it out first.
+Give a char set as long as your base.
+*/
+/*static*/ int uint_r_cvt_str(uint64_t n, int base, char* buf, char* charset) {
+	
+	int i = 0;
+	
+	while(n > 0) {
+		int64_t b = n / base;
+		int     a = n % base;
+		
+		buf[i++] = charset[a];
+		
+		n = b;
+	}
+	
+	return i;
+}
+
+/*
+Garbage in, garbage out. 
+Dont't give a base of 1.
+Dont't give a base greater than 36.
+Give a sufficiently long buffer. You figure it out first.
+Give a char set as long as your base.
+*/
+/*static*/ int int_r_cvt_str(int64_t n, int base, char* buf, char* charset) {
+	char negative = 0;
+	int i = 0;
+	
+	if(n < 0) {
+		negative = 1;
+		n = -n;
+	}
+	
+	i = uint_r_cvt_str(n, base, buf, charset);
+	
+	if(negative) buf[i++] = '-';
+	
+	return i;
+}
+
+/*static*/ int flt_r_cvt_str(float f, int base, char* buf, char* charset) {
+	int i = 0;
+	
+	uint32_t nf = *((uint32_t*)&f); 
+	
+	if(nf == 0x7f800000) { // infinity
+		strncpy(buf, "infinity", strlen("infinity"));
+		return strlen("infinity");
+	}
+	if(nf == 0xff800000) { // -infinity
+		strncpy(buf, "infinity", strlen("infinity"));
+		return strlen("infinity") + 1;
+	}
+	
+	uint32_t sign =  (nf & 0x80000000) >> 31;
+	uint32_t exp_r = (nf & 0x7ff00000) >> 20;
+	uint32_t frac =   nf & 0x000fffff;
+	int32_t exp = exp_r - 127;
+	
+	printf("%x, %x, %x\n", sign, exp_r, frac);
+	
+	(void)base;
+	(void)buf;
+	(void)charset;
+	(void)exp;
+	
+	
+	
+// 	while(n > 0) {
+// 		int64_t b = n / base;
+// 		int     a = n % base;
+// 		
+// 		buf[i++] = charset[a];
+// 		
+// 		n = b;
+// 	}
+	
+// 	if(negative) buf[i++] = '-';
+	
+	return i;
+}
+
+/*
+Garbage in, garbage out. 
+Dont't give a base of 1.
+Dont't give a base greater than 36.
+Give a sufficiently long buffer. You figure it out first.
+*/
+/*static*/ int int_r_cvt(int64_t n, int base, int upper, char* buf) {
+	
+	if(n == 0) {
+		buf[0] = '0';
+		return 1;
+	}
+	
+	static char* lchars = "0123456789abcdefghijklmnopqrstuvwxyz"; 
+	static char* uchars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	return int_r_cvt_str(n, base, buf, upper ? uchars : lchars);
+}
+
+
+int iprintf(char* fmt, ...) {
+	va_list va;
+	
+	va_start(va, fmt);
+	int n = 0;
+	
+	for(size_t i = 0; fmt[i]; i++) { 
+		
+		char c = fmt[i];
+		if(c == '%') {
+			// %[flags][width][.precision][length]specifier
+			
+			char uppercase = 0;
+// 			char left_justify = 0;
+// 			char force_sign = 0;
+// 			char optional_sign = 0;
+// 			char force_decimal = 0;
+// 			char pad_zeroes = 0;
+// 			char vector_print = 0;
+			
+// 			int width = 0;
+// 			int precision = 0;
+			
+			c = fmt[++i];
+			
+			if(c == 0) break;
+			if(c == '%') {
+				putc('%', stdout); 
+				continue;
+			}
+			
+			// flags
+			while(1) {
+				if(c == 0) { // end of fmt string
+					goto END_STR;
+				}
+				else if(c == '-') { // left-justify
+					c = fmt[++i];
+				}
+				else if(c == '+') { // show sign
+					c = fmt[++i];
+				}
+				else if(c == ' ') { // optional sign
+					c = fmt[++i];
+				}
+				else if(c == '#') { // force decimal point
+					c = fmt[++i];
+				}
+				else if(c == '0') { // left-pad zeroes
+					c = fmt[++i];
+				}
+				else if(c == 'v') { // vector
+					c = fmt[++i];
+				}
+				else break;
+			}
+			
+			// width
+			if(c == '*') { // specified in args
+				i++;
+				// va_arg(va, n);
+				n++;
+			}
+			else if(c >= '0' && c <= '9') {
+				while(c >= '0' && c <= '9') {
+					c = fmt[i++];
+					if(c == 0) goto END_STR;
+				}
+			}
+			
+			// precision
+			if(c == '.') {
+				c = fmt[++i];
+			
+				if(c == 0) goto END_STR;
+				else if(c == '*') { // specified in args
+					n++;
+					// va_arg(va, n);
+				}
+				else {
+					while(c >= '0' && c <= '9') {
+						c = fmt[i++];
+						if(c == 0) goto END_STR;
+					}
+				}
+			}
+			
+			// length 
+			if(c == 0) goto END_STR;
+			else if(c == 'h') {
+				c = fmt[i++];
+			}
+			else if(c == 'l') {
+				c = fmt[i++];
+			}
+			else if(c == 'z') {
+				c = fmt[i++];
+			}
+			else if(c == 't') {
+				c = fmt[i++];
+			}
+			else if(c == 'j') {
+				c = fmt[i++];
+			}
+			else if(c == 'L') {
+				c = fmt[i++];
+			}
+			
+			// specifier
+			switch(c) {
+				case 0: goto END_STR;
+				
+				case 'd': 
+				case 'i': // signed decimal int
+					break;
+				
+				case 'u': // unsigned decimal int
+					break;
+				
+				case 'o': // unsigned octal int 
+					
+					break;
+				
+				case 'X': uppercase = 1; 
+				case 'x': // unsigned hex int
+					break;
+				
+				case 'F': uppercase = 1; 
+				case 'f': // float, decimal
+					break;
+				
+				case 'E': uppercase = 1; 
+				case 'e': // decimal scientific notation
+					break;
+				
+				case 'G': uppercase = 1; 
+				case 'g': // shortest of e/f
+					break;
+				
+				case 'A': uppercase = 1; 
+				case 'a': // hex float
+					break;
+				
+				case 'c': // character 
+					break;
+				
+				case 's': // string
+					break;
+					
+				case 'P': // shortened pointer
+				case 'p': // pointer
+					break;
+					
+				case 'n': // set arg to number of chars written
+					break;
+					
+				default: 
+					break;
+			}
+			
+			(void)uppercase;
+			
+		}
+		else {
+			putc(c, stdout);
+		}
+		
+
+		
+	}
+	
+END_STR:
+	
+	va_end(va);
+	
+	return 0;
+}
