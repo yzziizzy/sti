444289b2ea5542279e6c6089de0a219bb099e193
izzy <yzziizzy@gmail.com>
2021-01-08 19:21:13 -0700

    remove a bunch of trash

--- scripting/gen_parser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-../parser/parser_gen ./scripting_tokens.txt -e > lexer_enums.h
-../parser/parser_gen ./scripting_tokens.txt -d > lexer_enum_names.h
-../parser/parser_gen ./scripting_tokens.txt -c > lexer_csets.c
-../parser/parser_gen ./scripting_tokens.txt -s > lexer_switch.c
-
