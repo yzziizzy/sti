6badfa160977c6f86093d48219d0a460aa5e4d4e
izzy <izzy@urchinator.com>
2023-08-15 11:37:01 -0500

    fixes and additions to matrix functions

--- matrix.c
+++ matrix.c
@@ -3,6 +3,10 @@
 
 #include "matrix.h"
 
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h> // malloc
+#include <string.h> // memset
 
 
 
@@ -13,7 +17,7 @@
 sti_matrix* sti_matrix_new(int c, int r) {
 	sti_matrix* mat;
 	
-	mat = malloc(sizeof(*mat) + sizeof(mat->data) * r * c);
+	mat = malloc(sizeof(*mat) + sizeof(mat->data[0]) * r * c);
 	mat->r = r;
 	mat->c = c;
 	
@@ -38,6 +42,21 @@ sti_matrix* sti_matrix_copy(sti_matrix* m) {
 }
 
 
+
+// careful here...
+void sti_matrix_print(sti_matrix* m, FILE* f) {
+	for(long r = 0; r < m->r; r++) {
+		for(long c = 0; c < m->c; c++) {
+			fprintf(f, "%.2f ", m->data[c + m->c * r]);
+		}
+		
+		fprintf(f, "\n");
+	}
+}
+
+
+
+
 void sti_matrix_clear(sti_matrix* m) {
 	memset(m->data, 0, sizeof(m->data) * m->c * m->r);
 }
@@ -57,6 +76,12 @@ void sti_matrix_set(sti_matrix* m, float v) {
 	}
 }
 
+
+void sti_matrix_load(sti_matrix* m, float* v) {
+	memcpy(m->data, v, sizeof(m->data[0]) * m->c * m->r);
+}
+
+
 void sti_matrix_ident(sti_matrix* m) {
 	for(int i = 0; i < m->c; i++)
 	for(int j = 0; j < m->r; j++) {
@@ -65,18 +90,43 @@ void sti_matrix_ident(sti_matrix* m) {
 }
 
 
+
+void sti_matrix_rand(sti_matrix* m, float min, float max) {
+	long len = m->c * m->r;
+	float sz = max - min;
+	for(long n = 0; n < len; n++) {
+		float x = ((float)rand() * sz) / (float)RAND_MAX;
+		m->data[n] = min + x; 
+	}
+}
+
+
 void sti_matrix_transpose(sti_matrix* a, sti_matrix* out) {
-	float tmp;
 	
 	assert(a->c * a->r <= out->c * out->r);
 	
 	out->r = a->c;
 	out->c = a->r;
 	
-	for(int i = 0; i < a->c; i++)
-	for(int j = 0; j <= a->r; j++) {
-		M(out, i, j) = M(a, j, i);
+	for(int r = 0; r < a->r; r++)
+	for(int c = r; c < a->c; c++) {
+		float tmp;
+		if(c < a->c) tmp = M(a, c, r);
+		if(c < out->c) M(out, c, r) = M(a, r, c);
+		if(c < a->c) M(out, r, c) = tmp;
 	}
+}
+
+
+int sti_matrix_eq(sti_matrix* a, sti_matrix* b) {
+	if(a->r != b->r || a->c != b->c) return 0;
+	
+	long len = a->c * a->r;
+	for(long n = 0; n < len; n++) {
+		if(a->data[n] != b->data[n]) return 0; 
+	}
+	
+	return 1;
 }
 
 sti_matrix* sti_matrix_mul(sti_matrix* a, sti_matrix* b) {
@@ -94,14 +144,13 @@ sti_matrix* sti_matrix_mul(sti_matrix* a, sti_matrix* b) {
 // no checks for size match.
 void sti_matrix_mulp(sti_matrix* a, sti_matrix* b, sti_matrix* out) {
 	
-	memset(out->data, 0, sizeof(out->data) * out->c * out->r);
+	long klim = a->c;
 	
-	for(int j = 0; j < b->r; j++)
-	for(int i = 0; i < a->c; i++) {
-		for(int k = 0; k < a->r; k++) {
-			// a moves right in columns
-			// b moves down in rows
-			M(out, i, j) += M(a, k, i) * M(b, j, k);
+	for(int c = 0; c < b->c; c++)
+	for(int r = 0; r < a->r; r++) {
+		M(out, c, r) = 0;
+		for(int k = 0; k < klim; k++) {
+			M(out, c, r) += M(a, k, r) * M(b, c, k);
 		}
 	}
 }
@@ -114,17 +163,23 @@ sti_matrix* sti_matrix_mul_transb(sti_matrix* a, sti_matrix* b) {
 	if(a->c != b->c) return NULL;
 	
 	o = sti_matrix_new(b->r, a->r);
-	
-	memset(o->data, 0, sizeof(o->data) * o->c * o->r);
-	
-	for(int j = 0; j < b->r; j++)
-	for(int i = 0; i < a->c; i++) {
-		for(int k = 0; k < a->r; k++) {
-			M(o, i, j) += M(a, k, i) * M(b, j, k);
-		}
-	}
+	sti_matrix_mulp_transb(a, b, o);
 	
 	return o;
+}
+
+
+void sti_matrix_mulp_transb(sti_matrix* a, sti_matrix* b, sti_matrix* out) {
+	
+	long klim = a->c;
+	
+	for(int c = 0; c < b->r; c++)
+	for(int r = 0; r < a->r; r++) {
+		M(out, c, r) = 0;
+		for(int k = 0; k < klim; k++) {
+			M(out, c, r) += M(a, k, r) * M(b, k, c);
+		}
+	}
 }
 
 #define MIN(a, b) (a < b ? a : b)
@@ -153,7 +208,16 @@ void sti_matrix_sub(sti_matrix* a, sti_matrix* b, sti_matrix* out) {
 }
 
 
-void sti_matrix_scalar_mul(sti_matrix* a, float s, sti_matrix* out) {
+void sti_matrix_scalar_mul(sti_matrix* a, sti_matrix* b, sti_matrix* out) {
+
+	long sz = a->c * a->r;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = a->data[i] * b->data[i];
+	}
+}
+
+void sti_matrix_scale(sti_matrix* a, float s, sti_matrix* out) {
 	
 	long sz = a->c * a->r;
 	
@@ -161,6 +225,51 @@ void sti_matrix_scalar_mul(sti_matrix* a, float s, sti_matrix* out) {
 		out->data[i] = a->data[i] * s;
 	}
 }
+
+
+
+// apply e^a[n]
+void sti_matrix_exp(sti_matrix* a, sti_matrix* out) {
+	
+	long sz = a->c * a->r;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = expf(a->data[i]);
+	}
+}
+
+// simple flat sum of all values in the matrix
+float sti_matrix_sum(sti_matrix* a) {
+	
+	long sz = a->c * a->r;
+	float sum = 0;
+	
+	for(int i = 0; i < sz; i++) {
+		sum += a->data[i];
+	}
+	
+	return sum;
+}
+
+
+void sti_matrix_softmax(sti_matrix* a, sti_matrix* out) {
+	
+	long sz = a->c * a->r;
+	float sum = 0;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = expf(a->data[i]);
+		sum += out->data[i];
+	}
+	
+	float invsum = 1.0 / sum;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] *= invsum;
+	}
+}
+
+
 
 
 void sti_matrix_min(sti_matrix* a, float minval, sti_matrix* out) {
@@ -202,9 +311,54 @@ void sti_matrix_tanh_clamp(sti_matrix* a, sti_matrix* out) {
 }
 
 
+void sti_matrix_relu_0(sti_matrix* a, sti_matrix* out) {
+	
+	long sz = a->c * a->r;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = fmax(0, a->data[i]);
+	}
+}
+
+void sti_matrix_relu_half(sti_matrix* a, sti_matrix* out) {
+	
+	long sz = a->c * a->r;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = fmax(0, a->data[i] - .5f) + .5f;
+	}
+}
+
+void sti_matrix_relu_n(sti_matrix* a, float n, sti_matrix* out) {
+	
+	long sz = a->c * a->r;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = fmax(0, a->data[i] - n) + n;
+	}
+}
+
+void sti_matrix_silu(sti_matrix* a, sti_matrix* out) {
+	long sz = a->c * a->r;
+	
+	for(int i = 0; i < sz; i++) {
+		out->data[i] = a->data[i] / (1.f + expf(-a->data[i]));
+	}
+}
 
 
+// mean squared error: SUM( (a - b)^2 )
+float sti_matrix_mse(sti_matrix* a, sti_matrix* b) {
 
-
+	long sz = a->c * a->r;
+	
+	float sum = 0;
+	for(int i = 0; i < sz; i++) {
+		float x = a->data[i] - b->data[i];
+		sum += x * x;
+	}
+	
+	return sum / sz;
+}
 
 
--- matrix.h
+++ matrix.h
@@ -13,14 +13,14 @@ Unlike most of sti, the matrix library is explicitly prefixed
 with "sti_" because "matrix" is such a popular data type name
 in other libraries. I assume that there are people who will 
 want to cherry pick the sti matrix files, or who want to use 
-sti their own matrix functions but don't want to have to think
-about removing the sti versions.  
+sti with their own matrix functions but don't want to have to 
+think about removing the sti versions.  
 
 */
 
 
-// column major: [c1r1, c1r2, c1r3, ..., c2r1, c2r2, c2r3, ..., c3r1, c3r2, c3r3, ..., ...]
 
+#include <stdio.h> // fprintf
 
 
 typedef struct sti_matrix {
@@ -37,31 +37,61 @@ sti_matrix* sti_matrix_size_for_mul(sti_matrix* a, sti_matrix* b);
 
 sti_matrix* sti_matrix_copy(sti_matrix* m);
 
+// careful here...
+void sti_matrix_print(sti_matrix* m, FILE* f);
+
 void sti_matrix_clear(sti_matrix* m);
 void sti_matrix_set(sti_matrix* m, float v);
+void sti_matrix_load(sti_matrix* m, float* v);
 void sti_matrix_ident(sti_matrix* m);
+
+void sti_matrix_rand(sti_matrix* m, float min, float max);
 
 void sti_matrix_transpose(sti_matrix* a, sti_matrix* out);
 
+int sti_matrix_eq(sti_matrix* a, sti_matrix* b);
+
 // returns a newly allocated matrix of the proper size
 sti_matrix* sti_matrix_mul(sti_matrix* a, sti_matrix* b);
+// no checks for size match.
+void sti_matrix_mulp(sti_matrix* a, sti_matrix* b, sti_matrix* out);
 
 // multiplies a with the transpose of b
 sti_matrix* sti_matrix_mul_transb(sti_matrix* a, sti_matrix* b);
-
 // no checks for size match.
-void sti_matrix_mulp(sti_matrix* a, sti_matrix* b, sti_matrix* out);
+void sti_matrix_mulp_transb(sti_matrix* a, sti_matrix* b, sti_matrix* out);
+
 
 
 void sti_matrix_add(sti_matrix* a, sti_matrix* b, sti_matrix* out);
 
 void sti_matrix_sub(sti_matrix* a, sti_matrix* b, sti_matrix* out);
-void sti_matrix_scalar_mul(sti_matrix* a, float s, sti_matrix* out);
+
+void sti_matrix_scalar_mul(sti_matrix* a, sti_matrix* b, sti_matrix* out);
+void sti_matrix_scale(sti_matrix* a, float s, sti_matrix* out);
+
+// apply e^a[n]
+void sti_matrix_exp(sti_matrix* a, sti_matrix* out);
+
+// simple flat sum of all values in the matrix
+float sti_matrix_sum(sti_matrix* a);
+
+
+void sti_matrix_softmax(sti_matrix* a, sti_matrix* out);
+
 
 
 void sti_matrix_min(sti_matrix* a, float minval, sti_matrix* out);
 void sti_matrix_max(sti_matrix* a, float maxval, sti_matrix* out);
 void sti_matrix_clamp(sti_matrix* a, float minval, float maxval, sti_matrix* out);
+
+void sti_matrix_relu_0(sti_matrix* a, sti_matrix* out);
+void sti_matrix_relu_half(sti_matrix* a, sti_matrix* out);
+void sti_matrix_relu_n(sti_matrix* a, float n, sti_matrix* out);
+
+
+// mean squared error: SUM( (a - b)^2 )
+float sti_matrix_mse(sti_matrix* a, sti_matrix* b);
 
 
 #endif // __sti__matrix_h__
