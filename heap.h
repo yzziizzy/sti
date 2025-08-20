df17bad4eba1777becdcee66def8e054b56d250d
izzy <izzy@urchinator.com>
2025-08-20 11:08:43 -0600

    fix element sizing in heap

--- heap.h
+++ heap.h
@@ -38,20 +38,20 @@ do { \
 } while(0)
 
 // insert a value into the heap
-#define HEAP_insert(h, e) heap_insert_(&(h)->heap, (void*)(e), sizeof(*((h)->type)))
+#define HEAP_insert(h, e) heap_insert_(&(h)->heap, (void*)(e), sizeof(**((h)->type)))
 void heap_insert_(heap_* h, char* elem, size_t elem_sz);
 
 // get the value of the most extreme value without removing it
-#define HEAP_peek(h, e) heap_peek_(&(h)->heap, (void*)(e), sizeof(*((h)->type)))
+#define HEAP_peek(h, e) heap_peek_(&(h)->heap, (void*)(e), sizeof(**((h)->type)))
 int heap_peek_(heap_* h, char* out, size_t elem_sz);
 
 // remove the top, most extreme value from the heap
-#define HEAP_pop(h, e) heap_pop_(&(h)->heap, (void*)(e), sizeof(*((h)->type)))
+#define HEAP_pop(h, e) heap_pop_(&(h)->heap, (void*)(e), sizeof(**((h)->type)))
 int heap_pop_(heap_* h, char* out, size_t elem_sz);
 
 // pop the top element and insert a new one at the same time
 // faster than separate operations.
-#define HEAP_insert_pop(h, i, o) heap_insert_pop_(&(h)->heap, (void*)(i), (o), sizeof(*((h)->type)))
+#define HEAP_insert_pop(h, i, o) heap_insert_pop_(&(h)->heap, (void*)(i), (o), sizeof(**((h)->type)))
 void heap_insert_pop_(heap_* h, char* in, char* out, size_t elem_sz);
 
 
@@ -59,11 +59,11 @@ void heap_insert_pop_(heap_* h, char* in, char* out, size_t elem_sz);
 #define HEAP_free(h) heap_free_(&(h)->heap);
 void heap_free_(heap_* h);
 
-#define HEAP_delete(h, e) heap_delete_(&(h)->heap, (void*)(e), sizeof(*((h)->type)))
+#define HEAP_delete(h, e) heap_delete_(&(h)->heap, (void*)(e), sizeof(**((h)->type)))
 void heap_delete_(heap_* h, char* elem, size_t elem_sz);
 
 // returns -1 if not found, the item index otherwise
-#define HEAP_find(h, e) heap_find_(&(h)->heap, (void*)(e), sizeof(*((h)->type)))
+#define HEAP_find(h, e) heap_find_(&(h)->heap, (void*)(e), sizeof(**((h)->type)))
 ssize_t heap_find_(heap_* h, char* elem, size_t elem_sz);
 
 
